943a318aa927e1c932e73c9b2176a2d37723168d
G5andeepD <e19063@eng.pdn.ac.lk>
2023-05-29 11:10:14 +0530

    Demo security Application added

---
+++ Back-End/src/main/java/com/kassadin/security/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.kassadin.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
