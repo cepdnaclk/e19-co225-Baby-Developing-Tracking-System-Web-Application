d96e461352a3871aa5c90bfe9b23508618d5e429
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2023-06-02 19:13:46 +0530

    Updating services
    Co-Authored-By: 19Kaumini <115539904+19Kaumini@users.noreply.github.com>

--- Front-End/src/services/auth_header.jsx
+++ Front-End/src/services/auth_header.jsx
@@ -1,9 +1,14 @@
+// This module exports a function that returns the authorization header
 export const authHeader = () => {
+  // Retrieves the "user" object from the local storage and parses it
   const user = JSON.parse(localStorage.getItem("user"));
 
+  // Checks if the user and user token exist
   if (user && user.token) {
-    return { Authorization: 'Bearer ' + user.token };
+    // Returns an object with the authorization header containing the user token
+    return { Authorization: "Bearer " + user.token };
   } else {
+    // Returns an empty object if the user or user token is missing
     return {};
   }
 };
--- Front-End/src/services/post.service.jsx
+++ Front-End/src/services/post.service.jsx
@@ -3,10 +3,12 @@ import { authHeader } from "./auth_header";
 
 const API_URL = "http://10.30.118.183:8080/api/v1";
 
+// Retrieves all public posts by making a GET request to the API
 const getAllPublicPosts = () => {
   return axios.get(API_URL + "/public");
 };
 
+// Retrieves all private posts by making a GET request to the API with the authorization header
 const getAllPrivatePosts = () => {
   return axios.get(API_URL + "/demo-controller", { headers: authHeader() });
 };
@@ -16,6 +18,6 @@ const postService = {
   getAllPrivatePosts,
 };
 
-console.log(getAllPrivatePosts)
+console.log(getAllPrivatePosts);
 
-export default postService;  
+export default postService;
