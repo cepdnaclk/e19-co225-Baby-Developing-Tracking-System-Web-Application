674ba389cd64bc28860f05d4d2a2a646d12f88c5
G5andeepD <e19063@eng.pdn.ac.lk>
2023-06-08 08:46:42 +0530

    Changed Swagger Titles

--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/config/OpenApiConfig.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/config/OpenApiConfig.java
@@ -13,12 +13,12 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @OpenAPIDefinition(
         info = @Info(
                 contact = @Contact(
-                        name = "Group 16",
+                        name = "Idea 2 - Web - Group 9 - 16",
                         email = "e19008@eng.pdn.ac.lk"
 
                 ),
-                description = "API Documentation for Restaurant Management System",
-                title = "Restaurant Management API",
+                description = "API Documentation for Baby Development Tracking System",
+                title = "Baby Development Tracking System API",
                 version = "1.0",
 
                 termsOfService = "Terms of service"
