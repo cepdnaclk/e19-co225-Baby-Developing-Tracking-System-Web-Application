956b8f17899130e95fb386735ac1a5fb3d987237
G5andeepD <e19063@eng.pdn.ac.lk>
2023-06-11 22:25:59 +0530

    Added Role Enum

---
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Model/Role.java
@@ -0,0 +1,74 @@
+package com.babydevelopingtrackingsystem.Model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public enum Role {
+
+    USER(Collections.emptySet()),
+    ADMIN(
+            Set.of(
+                    ADMIN_READ,
+                    ADMIN_UPDATE,
+                    ADMIN_DELETE,
+                    ADMIN_CREATE,
+                    PARENT_READ,
+                    PARENT_UPDATE,
+                    PARENT_DELETE,
+                    PARENT_CREATE,
+                    DOCTOR_READ,
+                    DOCTOR_UPDATE,
+                    DOCTOR_DELETE,
+                    DOCTOR_CREATE,
+                    MIDWIFE_READ,
+                    MIDWIFE_UPDATE,
+                    MIDWIFE_DELETE,
+                    MIDWIFE_CREATE
+            )
+    ),
+    PARENT(
+            Set.of(
+                    PARENT_READ,
+                    PARENT_UPDATE,
+                    PARENT_DELETE,
+                    PARENT_CREATE
+            )
+    ),
+    DOCTOR(
+            Set.of(
+                    DOCTOR_READ,
+                    DOCTOR_UPDATE,
+                    DOCTOR_DELETE,
+                    DOCTOR_CREATE
+            )
+    ),
+    MIDWIFE(
+            Set.of(
+                    MIDWIFE_READ,
+                    MIDWIFE_UPDATE,
+                    MIDWIFE_DELETE,
+                    MIDWIFE_CREATE
+            )
+    ),
+
+    ;
+
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
+}
