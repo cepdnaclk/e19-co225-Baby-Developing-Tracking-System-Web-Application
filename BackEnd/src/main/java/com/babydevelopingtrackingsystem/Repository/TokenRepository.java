9fbabce49a54031bb89b506dbeaec313b0e480fe
G5andeepD <e19063@eng.pdn.ac.lk>
2023-06-12 09:56:54 +0530

    Added Application Config and Jwt Service

--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/Repository/TokenRepository.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Repository/TokenRepository.java
@@ -1,7 +1,11 @@
 package com.babydevelopingtrackingsystem.Repository;
 
 import com.babydevelopingtrackingsystem.Model.Token;
+import io.micrometer.observation.ObservationFilter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TokenRepository extends JpaRepository<Token,Integer> {
+    Optional<Token> findByToken(String jwt);
 }
--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/Repository/UserRepository.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.babydevelopingtrackingsystem.Repository;
 import com.babydevelopingtrackingsystem.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer>{
 
+    Optional<Object> findByEmail(String email);
 }
