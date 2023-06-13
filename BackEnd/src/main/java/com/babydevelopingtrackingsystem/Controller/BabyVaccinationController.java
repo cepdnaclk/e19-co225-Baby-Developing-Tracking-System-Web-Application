d0516f8084f57e6d61e58ed4ff4484eca23f3687
G5andeepD <e19063@eng.pdn.ac.lk>
2023-06-13 12:27:43 +0530

    Added Vaccine Alert System

--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/Controller/BabyVaccinationController.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Controller/BabyVaccinationController.java
@@ -2,10 +2,13 @@ package com.babydevelopingtrackingsystem.Controller;
 
 import com.babydevelopingtrackingsystem.Dto.BabyVaccinationRequest;
 import com.babydevelopingtrackingsystem.Model.BabyVaccination;
+import com.babydevelopingtrackingsystem.Model.VaccineAlert;
 import com.babydevelopingtrackingsystem.Service.BabyVaccinationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/add_vaccine")
@@ -25,5 +28,9 @@ public class BabyVaccinationController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(newBabyVaccine);
+    }
+    @GetMapping("alerts/{babyId}")
+    public ResponseEntity<List<VaccineAlert>> getVaccineAlerts(@PathVariable int babyId){
+        return ResponseEntity.ok(babyVaccinationService.getAlerts(babyId));
     }
 }
--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/Repository/BabyVaccinationRepository.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Repository/BabyVaccinationRepository.java
@@ -3,8 +3,16 @@ package com.babydevelopingtrackingsystem.Repository;
 import com.babydevelopingtrackingsystem.Model.Baby;
 import com.babydevelopingtrackingsystem.Model.BabyVaccination;
 import com.babydevelopingtrackingsystem.Model.Vaccination;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface BabyVaccinationRepository extends JpaRepository<BabyVaccination,Integer> {
     public boolean existsBabyVaccinationByBabyAndVaccination(Baby baby, Vaccination vaccination);
+
+    @Query("SELECT v FROM BabyVaccination v WHERE v.vaccinationDate <= :notificationDate AND v.vaccinationDate >= :today")
+    List<BabyVaccination> findUpcomingVaccineAlerts(LocalDate today, LocalDate notificationDate);
 }
