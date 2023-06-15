d5de7cee00f13b3d0b2a8818718f484443c73a84
Sachith Dissanayaka <e19087@eng.pdn.ac.lk>
2023-06-15 22:53:49 +0530

    Admin login page added

--- Front-End/src/AdminLogin.jsx
+++ Front-End/src/AdminLogin.jsx
@@ -3,16 +3,46 @@ import { useNavigate } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
-
 export const AdminLogin = (props) => {
-  
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (username === "root" && password === "password") {
+      navigate("/admin");
+    } else {
+      alert("Invalid credentials. Please try again.");
+    }
+  };
 
   return (
     <div className="add-doctor-container">
-      <Nav />
+      <Header />
+      <div className="add-doctor-form-container">
+        <h1 className="h1">Admin Login</h1>
+        <form className="add-doctor-form">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-    
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+      </div>
       <Footer />
     </div>
   );
