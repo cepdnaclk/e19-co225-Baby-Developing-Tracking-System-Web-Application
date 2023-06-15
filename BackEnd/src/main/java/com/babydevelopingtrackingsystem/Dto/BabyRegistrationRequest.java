9beca45bfb99408685be27380caf2304349430e4
Pawani Thamaya <e19264@eng.pdn.ac.lk>
2023-06-15 13:52:04 +0530

    dto model for baby registration

--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/Dto/BabyRegistrationRequest.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Dto/BabyRegistrationRequest.java
@@ -1,2 +1,20 @@
-package com.babydevelopingtrackingsystem.Dto;public class BabyRegistrationRequest {
+package com.babydevelopingtrackingsystem.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class BabyRegistrationRequest {
+    private String firstName;
+    private String lastName;
+
+    private String gender;
+
+    private LocalDate birthday;
 }
