191f645ff0ab504ddbc2d21b82c90098be19b418
Sachith Dissanayaka <e19087@eng.pdn.ac.lk>
2023-06-15 22:43:25 +0530

    Admin Login Page Added

---
+++ Front-End/src/AdminLogin.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+
+
+export const AdminLogin = (props) => {
+  
+
+  return (
+    <div className="add-doctor-container">
+      <Header />
+
+    
+
+      <Footer />
+    </div>
+  );
+};
