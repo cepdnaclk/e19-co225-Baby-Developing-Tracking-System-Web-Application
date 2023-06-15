a90002ee03c43d8cb998e250977b61fe5456393a
Pawani Thamaya <e19264@eng.pdn.ac.lk>
2023-06-15 13:52:59 +0530

    Parent Service Basic

--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/Service/ParentService.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Service/ParentService.java
@@ -1,2 +1,44 @@
-package com.babydevelopingtrackingsystem.Service;public class ParentService {
+package com.babydevelopingtrackingsystem.Service;
+
+import com.babydevelopingtrackingsystem.Dto.BabyRegistrationRequest;
+import com.babydevelopingtrackingsystem.Model.Baby;
+import com.babydevelopingtrackingsystem.Model.Parent;
+import com.babydevelopingtrackingsystem.Repository.BabyRepository;
+import com.babydevelopingtrackingsystem.Repository.ParentRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParentService {
+    private final ParentRepository parentRepository;
+    private final BabyRepository babyRepository;
+
+    public ParentService(ParentRepository parentRepository, BabyRepository babyRepository) {
+        this.parentRepository = parentRepository;
+        this.babyRepository = babyRepository;
+    }
+
+    public boolean doesBabyExistForParent() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String email = authentication.getName();
+
+        Parent parent = parentRepository.findByEmail(email);
+
+        return babyRepository.existsByParent(parent);
+    }
+
+    public void addNewBaby(BabyRegistrationRequest babyRegistrationRequest) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String email = authentication.getName();
+
+        Parent parent = parentRepository.findByEmail(email);
+        Baby baby = new Baby();
+
+        baby.setParent(parent);
+        baby.setDateofBirth(babyRegistrationRequest.get);
+
+    }
 }
