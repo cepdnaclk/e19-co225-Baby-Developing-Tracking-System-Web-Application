7c2031573db8d694ecd621f6dcda6599290b2bca
Pawani Thamaya <e19264@eng.pdn.ac.lk>
2023-06-15 13:54:57 +0530

    small changes

--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/Repository/BabyRepository.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Repository/BabyRepository.java
@@ -1,6 +1,7 @@
 package com.babydevelopingtrackingsystem.Repository;
 
 import com.babydevelopingtrackingsystem.Model.Baby;
+import com.babydevelopingtrackingsystem.Model.Parent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +11,6 @@ import java.util.List;
 public interface BabyRepository extends JpaRepository<Baby,Integer> {
     @Query("SELECT b FROM Baby b WHERE b.doctor.id = :id")
     List<Baby> findAllByDoctor(@Param("id") Integer id);
+
+    boolean existsByParent(Parent parent);
 }
