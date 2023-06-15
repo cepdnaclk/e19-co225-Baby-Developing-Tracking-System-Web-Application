7980315c486e1ca0c3222febb8c9962a3690a1a8
Pawani Thamaya <e19264@eng.pdn.ac.lk>
2023-06-15 13:54:35 +0530

    Controller

--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/Controller/ParentController.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Controller/ParentController.java
@@ -1,4 +1,28 @@
 package com.babydevelopingtrackingsystem.Controller;
 
+import com.babydevelopingtrackingsystem.Dto.BabyRegistrationRequest;
+import com.babydevelopingtrackingsystem.Service.ParentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/parent")
 public class ParentController {
+    private final ParentService parentService;
+
+    public ParentController(ParentService parentService) {
+        this.parentService = parentService;
+    }
+
+    @GetMapping("/babyNotExist")
+    public boolean babyNotExist(){
+       return !parentService.doesBabyExistForParent();
+    }
+
+    public void registerBaby(@RequestBody BabyRegistrationRequest babyRegistrationRequest){
+        parentService.addNewBaby(babyRegistrationRequest);
+
+    }
 }
