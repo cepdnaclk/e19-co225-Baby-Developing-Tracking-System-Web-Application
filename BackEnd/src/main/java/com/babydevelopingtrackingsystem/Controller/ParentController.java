fca050e808595b037a28002361b3e35214ba8757
Pawani Thamaya <e19264@eng.pdn.ac.lk>
2023-06-15 14:04:08 +0530

    Added new dateformat converter

--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/Controller/ParentController.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Controller/ParentController.java
@@ -2,10 +2,7 @@ package com.babydevelopingtrackingsystem.Controller;
 
 import com.babydevelopingtrackingsystem.Dto.BabyRegistrationRequest;
 import com.babydevelopingtrackingsystem.Service.ParentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/parent")
@@ -20,7 +17,7 @@ public class ParentController {
     public boolean babyNotExist(){
        return !parentService.doesBabyExistForParent();
     }
-
+    @PostMapping("/registerBaby")
     public void registerBaby(@RequestBody BabyRegistrationRequest babyRegistrationRequest){
         parentService.addNewBaby(babyRegistrationRequest);
 
