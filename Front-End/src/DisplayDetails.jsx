97f06fcc85d6975e391d24e5050484c312d9dfe5
akilagn <115577155+akilagn@users.noreply.github.com>
2023-06-16 23:25:06 +0530

    Add the Details page

--- Front-End/src/DisplayDetails.jsx
+++ Front-End/src/DisplayDetails.jsx
@@ -1,79 +1,145 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Header } from "./Header";
+import React, { useState, useEffect } from "react";
 import { Footer } from "./Footer";
-import "./DisplayDetails.css";
+import { Header } from "./Header";
+import "./Display.css";
 
-export const DisplayDetails = (props) => {
-  const [userDetails] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    role: ""
-  });
-
-  const [babyDetails] = useState({
-    id: 1,
-    Name: "",
-    DateofBirth: "",
-    Gender: "",
-    BloodType: "",
-    BirthWeight: 0,
-    BirthLength: 0,
-    EyeColor: "",
-    HairColor: "",
-    SkinColor: "",
-    Nationality: "",
-    BirthPlace: "",
-    BirthHospital: "",
-    ParentInformation: "",
-    ContactInformation: "",
-    MedicalConditions: "",
-    Allergies: "",
-    ImmunizationRecords: "",
-    GrowthRecords: "",
-    DevelopmentalMilestones: ""
-  });
-
-  return (
-    <div className="details-container">
-      <Header />
-
-      <div className="card user-details">
-        <h2>User Details</h2>
-        <p className="detail">First Name: {userDetails.firstName}</p>
-        <p className="detail">Last Name: {userDetails.lastName}</p>
-        <p className="detail">Email: {userDetails.email}</p>
-        <p className="detail">Password: {userDetails.password}</p>
-        <p className="detail">Role: {userDetails.role}</p>
+export const DisplayDetails = () => {
+    const [userData, setUserData] = useState({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      role: ""
+    });
+  
+    const [babyData, setBabyData] = useState({
+      name: "",
+      dateOfBirth: "",
+      gender: "",
+      bloodType: "",
+      birthWeight: 0,
+      birthLength: 0,
+      eyeColor: "",
+      hairColor: "",
+      skinColor: "",
+      nationality: "",
+      birthPlace: "",
+      birthHospital: "",
+      parentInformation: "",
+      contactInformation: "",
+      medicalConditions: "",
+      allergies: "",
+      immunizationRecords: "",
+      growthRecords: "",
+      developmentalMilestones: ""
+    });
+  
+    const handleFlightUpdate = (flightId) => {
+      // Handle flight update for the given flightId
+      console.log("Flight update for ID:", flightId);
+    };
+  
+    const handleFlightDelete = (flightId) => {
+      FlightService.deleteFlight(flightId) // Call the deleteFlight method from 'auth'
+        .then((data) => {
+          console.log("Flight delete response:", data);
+          // Update the flightData state by filtering out the deleted flight
+          setFlightData((prevFlightData) =>
+            prevFlightData.filter((flight) => flight.flightId !== flightId)
+          );
+        })
+        .catch((error) => {
+          console.error("Flight delete error:", error);
+          // Handle any error that occurred during the delete request
+        });
+    };
+  
+    const handleAirportUpdate = (airportCode) => {
+      // Handle airport update for the given airportCode
+      console.log("Airport update for Code:", airportCode);
+    };
+  
+    const handleAirportDelete = (airportCode) => {
+      // Handle airport deletion for the given airportCode
+      console.log("Airport delete for Code:", airportCode);
+    };
+  
+    return (
+      <div>
+        <Header />
+        <div className="container">
+          <h2 className="h2">USER DETAILS</h2>
+          <table className="table">
+            <thead>
+              <tr className="tr">
+                <th className="th">First Name</th>
+                <th className="th">Last Name</th>
+                <th className="th">Email</th>
+                <th className="th">Role</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="tr">
+                <td className="td">{userData.firstName}</td>
+                <td className="td">{userData.lastName}</td>
+                <td className="td">{userData.email}</td>
+                <td className="td">{userData.role}</td>
+              </tr>
+            </tbody>
+          </table>
+  
+          <h2 className="h2">BABY DETAILS</h2>
+          <table className="table">
+            <thead>
+              <tr className="tr">
+                <th className="th">Name</th>
+                <th className="th">Date of Birth</th>
+                <th className="th">Gender</th>
+                <th className="th">Blood Type</th>
+                <th className="th">Birth Weight</th>
+                <th className="th">Birth Length</th>
+                <th className="th">Eye Color</th>
+                <th className="th">Hair Color</th>
+                <th className="th">Skin Color</th>
+                <th className="th">Nationality</th>
+                <th className="th">Birth Place</th>
+                <th className="th">Birth Hospital</th>
+                <th className="th">Parent Information</th>
+                <th className="th">Contact Information</th>
+                <th className="th">Medical Conditions</th>
+                <th className="th">Allergies</th>
+                <th className="th">Immunization Records</th>
+                <th className="th">Growth Records</th>
+                <th className="th">Developmental Milestones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="tr">
+                <td className="td">{babyData.name}</td>
+                <td className="td">{babyData.dateOfBirth}</td>
+                <td className="td">{babyData.gender}</td>
+                <td className="td">{babyData.bloodType}</td>
+                <td className="td">{babyData.birthWeight}</td>
+                <td className="td">{babyData.birthLength}</td>
+                <td className="td">{babyData.eyeColor}</td>
+                <td className="td">{babyData.hairColor}</td>
+                <td className="td">{babyData.skinColor}</td>
+                <td className="td">{babyData.nationality}</td>
+                <td className="td">{babyData.birthPlace}</td>
+                <td className="td">{babyData.birthHospital}</td>
+                <td className="td">{babyData.parentInformation}</td>
+                <td className="td">{babyData.contactInformation}</td>
+                <td className="td">{babyData.medicalConditions}</td>
+                <td className="td">{babyData.allergies}</td>
+                <td className="td">{babyData.immunizationRecords}</td>
+                <td className="td">{babyData.growthRecords}</td>
+                <td className="td">{babyData.developmentalMilestones}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <Footer />
       </div>
-
-      <div className="card baby-details">
-        <h2>Baby Details</h2>
-        <p className="detail">ID: {babyDetails.id}</p>
-        <p className="detail">Name: {babyDetails.Name}</p>
-        <p className="detail">Date of Birth: {babyDetails.DateofBirth}</p>
-        <p className="detail">Gender: {babyDetails.Gender}</p>
-        <p className="detail">Blood Type: {babyDetails.BloodType}</p>
-        <p className="detail">Birth Weight: {babyDetails.BirthWeight}</p>
-        <p className="detail">Birth Length: {babyDetails.BirthLength}</p>
-        <p className="detail">Eye Color: {babyDetails.EyeColor}</p>
-        <p className="detail">Hair Color: {babyDetails.HairColor}</p>
-        <p className="detail">Skin Color: {babyDetails.SkinColor}</p>
-        <p className="detail">Nationality: {babyDetails.Nationality}</p>
-        <p className="detail">Birth Place: {babyDetails.BirthPlace}</p>
-        <p className="detail">Birth Hospital: {babyDetails.BirthHospital}</p>
-        <p className="detail">Parent Information: {babyDetails.ParentInformation}</p>
-        <p className="detail">Contact Information: {babyDetails.ContactInformation}</p>
-        <p className="detail">Medical Conditions: {babyDetails.MedicalConditions}</p>
-        <p className="detail">Allergies: {babyDetails.Allergies}</p>
-        <p className="detail">Immunization Records: {babyDetails.ImmunizationRecords}</p>
-        <p className="detail">Growth Records: {babyDetails.GrowthRecords}</p>
-        <p className="detail">Developmental Milestones: {babyDetails.DevelopmentalMilestones}</p>
-      </div>
-
-      <Footer />
-    </div>
-  );
-};
+    );
+  };
+  
