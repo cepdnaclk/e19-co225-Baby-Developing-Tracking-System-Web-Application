3e44cb3b6bfba3990c38de697f06beb3c0022caa
Sachith Dissanayaka <e19087@eng.pdn.ac.lk>
2023-06-17 13:49:12 +0530

    Update AddParentService.java

--- BackEnd/src/main/java/com/babydevelopingtrackingsystem/Service/AddParentService.java
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Service/AddParentService.java
@@ -1,4 +1,21 @@
 package com.babydevelopingtrackingsystem.Service;
+import com.babydevelopingtrackingsystem.Dto.DoctorDto;
+import com.babydevelopingtrackingsystem.Model.Doctor;
+import com.babydevelopingtrackingsystem.Repository.DoctorRepository;
+import com.babydevelopingtrackingsystem.Utill.VariableList;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
 public class AddParentService {
+
+
+
 }
