f50d7318ad6521be2c53ae73e194a7da8cc157a4
19Kaumini <e19009@eng.pdn.ac.lk>
2023-06-23 12:51:28 +0530

    Add Appoinment exceptions files

---
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Exceptions/AppointmentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.babydevelopingtrackingsystem.Exceptions;
+
+public class AppointmentNotFoundException extends AppointmentException {
+
+    public AppointmentNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ BackEnd/src/main/java/com/babydevelopingtrackingsystem/Exceptions/InvalidAppointmentException.java
@@ -0,0 +1,8 @@
+package com.babydevelopingtrackingsystem.Exceptions;
+
+public class InvalidAppointmentException extends AppointmentException {
+
+    public InvalidAppointmentException(String message) {
+        super(message);
+    }
+}
