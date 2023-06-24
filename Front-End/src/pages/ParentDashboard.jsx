b5e549bac4a71034e40bc430b2217f7bdafe12ab
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2023-06-24 17:40:32 +0530

    ParentBabyDetailsCard, DoctorBabyDetailsCard, ParentDashboard

--- Front-End/src/pages/ParentDashboard.jsx
+++ Front-End/src/pages/ParentDashboard.jsx
@@ -1,80 +1,46 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import AppointmentDetailsCard from "../components/AppointmentDetailsCard";
 import { Nav } from "../Nav";
 import { Footer } from "../Footer";
 import Calendar from "../components/Calender";
 import { useNavigate } from "react-router-dom";
 import "./ParentDashboard.css";
-import "../components/AppointmentDetailsCard.css";
 import ParentBabyDetailsCard from "../components/ParentBabyDetailsCard";
 import BabyWeight from "../components/WeightChart";
 import BabyHeight from "../components/HeightChart";
 
 const ParentDashboard = () => {
   const [selectedBaby, setSelectedBaby] = useState(null);
-  const [selectedAppointment, setSelectedAppointment] = useState(null);
   const [isBabyNotAdded, setIsBabyAdded] = useState(true);
   const [selectedTab, setSelectedTab] = useState("height");
 
   const navigate = useNavigate();
 
   // Sample data for the baby table
-  const babyTableData = [
+  const demoBaby =
     {
-      name: "Alex Peter",
-      parentName: "Jenny Fernandes",
-      sex: "Male",
-      appointedDoctor: "Doctor 1",
-      hasAppointment: true,
-      appointmentDate: "2023-06-30",
-    },
-    {
-      name: "Baby 2",
-      parentName: "Parent 2",
-      sex: "Female",
-      appointedDoctor: "Doctor 2",
-      hasAppointment: false,
-    },
-    // Add more baby data as needed
-  ];
-  const demoBaby = {
-    name: "Peter",
-    age: 2,
-    parentName: "John",
-    address: "Kandy",
-    specialInformation: "Cute",
-  };
-  const [selectedBabyTableData, setSelectedBabyTableData] =
-    useState(babyTableData);
+      id: "1",
+      babyName: "Jenny Fernandes",
+      midwifeName: "Alex Peter",
+      doctorName: "Doctor 1",
+      gender: "Male",
+      babyVaccinations: [
+        {
+          vaccineName: "Moderna",
+          dueDate: "2023-06-24",
+          status: "Pending"
+        },
+        {
+          vaccineName: "Ado Naa",
+          dueDate: "2023-07-24",
+          status: "Completed"
+        }]
+    };
+  
+  
   const [babyData, setBabyData] = useState(demoBaby);
-  // // Uncomment this to connect the table with the database
-  // //(Note that the fields are not correctly matching at the moment)
-  // //(Change the endpoint and fields as necessary)
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const token = JSON.parse(localStorage.getItem("user"));
-  //       const access = token.access_token;
-  //       console.log(access);
-
-  //       const response = await axios.get('http://localhost:8080/api/v1/doctor', {
-  //         headers: {
-  //           "Access-Control-Allow-Origin": true,
-  //           Authorization: "Bearer " + access
-  //         },
-  //       });
-
-  //       setSelectedBabyTableData(response.data);
-  //       console.log(response.data);
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
+  console.log(babyData)
+  
 
   // // Method to find out whether you have already registered a baby or not
   useEffect(() => {
@@ -132,38 +98,16 @@ const ParentDashboard = () => {
     fetchData();
   }, []);
 
-  // Sample data for the calendar
-  const calendarData = [
-    {
-      date: "2023-06-08",
-      works: ["Work 1", "Work 2"],
-    },
-    {
-      date: "2023-06-09",
-      works: ["Work 3"],
-    },
-    // Add more calendar data as needed
-  ];
-
-  const handleBabyRowClick = (baby) => {
+  const handleBabyClick = (baby) => {
     setSelectedBaby(baby);
+
+    console.log("Clicked")
+    console.log(baby)
+    
   };
 
-  const handleAppointmentButtonClick = (appointment) => {
-    setSelectedAppointment(appointment);
+  const handleCloseCard = () => {
     setSelectedBaby(null);
-  };
-
-  const handleAppointmentCardClose = () => {
-    setSelectedAppointment(null);
-  };
-
-  const handleCalendarItemClick = (works) => {
-    setSelectedAppointment(works);
-  };
-
-  const handleCalendarCardClose = () => {
-    setSelectedAppointment(null);
   };
 
   const handleTabChange = (tab) => {
@@ -188,7 +132,7 @@ const ParentDashboard = () => {
             </button>
           )}
 
-          {!isBabyNotAdded && <ParentBabyDetailsCard baby={babyData} />}
+          {/* {!isBabyNotAdded && <ParentBabyDetailsCard baby={babyData} />} */}
 
           <div className="tabs-container w-full">
             <button
@@ -215,14 +159,12 @@ const ParentDashboard = () => {
             >
               Calendar
             </button>
-            <button
-              className={`tab-button ${
-                selectedTab === "appointment" ? "active" : ""
-              }`}
-              onClick={() => handleTabChange("appointment")}
+            {!isBabyNotAdded && <button
+              className="tab-button"
+              onClick={() => handleBabyClick(babyData)}
             >
-              Appointment Table
-            </button>
+              Baby Details
+            </button>}
           </div>
 
           {selectedTab === "height" && (
@@ -243,87 +185,16 @@ const ParentDashboard = () => {
           )}
           {selectedTab === "calendar" && <Calendar />}
           <button className="SpaceButton"></button>
-          {selectedTab === "appointment" && (
-            <div className="baby-table sm:mx-10 my-10 scale-60 sm:scale-100 sm:border rounded-lg sm:p-8 sm:pb-12">
-              <table className="w-full table-fixed border-collapse rounded-lg">
-                <thead>
-                  <tr>
-                    <th className="bg-blue-200 font-bold py-2">
-                      <button>Baby Name</button>
-                    </th>
-                    <th className="bg-blue-200 font-bold py-2">
-                      <button>Parent Name</button>
-                    </th>
-                    <th className="bg-blue-200 font-bold py-2">
-                      <button>Sex</button>
-                    </th>
-                    <th className="bg-blue-200 font-bold py-2">
-                      <button>Allocated Doctor</button>
-                    </th>
-                    <th className="bg-blue-200 font-bold py-2">
-                      <button>Appointment</button>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {selectedBabyTableData.map((baby) => (
-                    <tr
-                      key={baby.name}
-                      className="cursor-pointer hover:bg-gray-100"
-                    >
-                      <td
-                        className="border py-2 px-3 text-center"
-                        onClick={() => handleBabyRowClick(baby)}
-                      >
-                        {baby.name}
-                      </td>
-                      <td
-                        className="border py-2 px-3 text-center"
-                        onClick={() => handleBabyRowClick(baby)}
-                      >
-                        {baby.parentName}
-                      </td>
-                      <td
-                        className="border py-2 px-3 text-center"
-                        onClick={() => handleBabyRowClick(baby)}
-                      >
-                        {baby.sex}
-                      </td>
-                      <td
-                        className="border py-2 px-3 text-center"
-                        onClick={() => handleBabyRowClick(baby)}
-                      >
-                        {baby.appointedDoctor}
-                      </td>
-                      <td className="border py-2 px-2 text-center">
-                        {baby.hasAppointment && (
-                          <button
-                            className="appointment-button blink"
-                            onClick={() => handleAppointmentButtonClick(baby)}
-                          >
-                            Appointment Requested
-                          </button>
-                        )}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          )}
+          {selectedBaby && (
+          
+            <ParentBabyDetailsCard
+              baby={selectedBaby}
+              onClose={() => setSelectedBaby(null)}
+            />
+          )} 
         </div>
-        {selectedBaby && (
-          <BabyDetailsCard
-            baby={selectedBaby}
-            onClose={() => setSelectedBaby(null)}
-          />
-        )}
-        {selectedAppointment && (
-          <AppointmentDetailsCard
-            appointment={selectedAppointment}
-            onClose={handleAppointmentCardClose}
-          />
-        )}
+        
+         
       </div>
       <Footer />
     </div>
